9d27eb918da4a30d2ac98ed1abe5570df1932633
Alex B <ale_bordo@hotmail.it>
2020-07-20 15:48:17 +0200

    Primo commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index(){
+            return Content("Ok");            
+        }
+
+        public IActionResult Detail(string id){
+            return Content("Ok. ID = " + id);            
+        }
+
+        public IActionResult DetailJSON(string id){
+            return Json("Ok. ID = " + id);
+        }
+    }
+}
